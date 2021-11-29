2a046eeedb12882ed620d664906db64057b23d53
Doni <donihernandez003@gmail.com>
2021-11-28 20:01:59 -0500

    caritem and styled button components

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {View, Text, ImageBackground} from 'react-native'
 import styles  from './styles'
+import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
     return (
@@ -10,6 +11,20 @@ const CarItem = (props) => {
                     <Text style={styles.title}>Model S</Text>
                     <Text style={styles.subtitle}>Starting at $69,420</Text>
                 </View>
+
+                <StyledButton
+                    type="primary"
+                    content={"Custom Order"}
+                    onPress={() => {
+                    console.warn('Custom Order was pressed')
+                }}/>
+                <StyledButton
+                    type="secondary"
+                    content={"Existing Inventory"}
+                    onPress={() => {
+                        console.warn('Existing Inventory was pressed')
+                    }}
+                />
             </ImageBackground>
         </View>
     )
---
+++ components/StyledButton/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {View, Text, Pressable} from 'react-native'
+import styles  from './styles'
+
+const StyledButton = ({ type, content, onPress }) => {
+    const backgroundColor = type === 'primary' ? '#333' : 'white'
+    const textColor = type === 'primary' ? 'white' : '#333'
+
+    console.warn(textColor)
+
+    return (
+        <View style={styles.container}>
+            <Pressable style={[styles.button, { backgroundColor: backgroundColor }]}
+                       onPress={ () => {
+                           console.warn('Hey there')
+                       }}
+            >
+                <Text style={[styles.text, {color: textColor}]}>{ content }</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default StyledButton
